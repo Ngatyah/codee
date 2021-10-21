d99386962384e058b659ef20956483681640020f
Ngatyah <davidngatyah@gmail.com>
2021-10-21 22:35:29 +0300

    permission

--- lib/file_utils.dart
+++ lib/file_utils.dart
@@ -1,16 +1,22 @@
 // ignore_for_file: avoid_print
 
 import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart';
+
 class FileUtils {
-  static Future<String> get getFilePath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
+  static Future<String?> get getFilePath async {
+    final directory = await getExternalStorageDirectory();
+    return directory?.path;
   }
 
   static Future<File> get getFile async {
-    final path = await getFilePath;
-    return File('$path/${DateTime.now().millisecondsSinceEpoch}.txt');
+    var status = await Permission.manageExternalStorage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    } 
+      final path = await getFilePath;
+      return File('$path/${DateTime.now().millisecondsSinceEpoch}.txt');
   }
 
   static Future<File> saveToFile(String data) async {
@@ -20,8 +26,8 @@ class FileUtils {
   }
 
   static Future<String> readFiles() async {
-      final file = await getFile;
-      String data = await file.readAsString();
-      return data;
+    final file = await getFile;
+    String data = await file.readAsString();
+    return data;
   }
 }
