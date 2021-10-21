1112bbb00ece125d56ada22bd8ebf8b9820c8e2c
Ngatyah <davidngatyah@gmail.com>
2021-10-21 08:34:17 +0300

    Solved the issue with data display

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,25 +20,31 @@ class _HomepageState extends State<Homepage> {
   }
 
   onMessage(SmsMessage message) async {
-    messages.add(message);
+    setState(() {
+      messages = [message, ...messages];
+    });
   }
 
   backgroundMessage(dynamic sms) async {
     var backgroundSms = widget.backgroundMessageHandler();
-    messages.add(backgroundSms);
-  }
-
-  Future<void> allMessage() async {
-    allMessages = await telephony.getInboxSms();
-    for (var message in allMessages) {
-      messages.add(message);
-    }
+    setState(() {
+      messages = [backgroundSms, ...messages];
+    });
   }
 
   Future<void> initPlatformState() async {
     final bool? result = await telephony.requestPhoneAndSmsPermissions;
+    print("$result..==================================");
+    messages = await telephony.getInboxSms();
+    debugPrint(messages.first.body);
+    setState(() {
+      messages = messages;
+    });
 
     if (result != null && result) {
+      debugPrint(
+          '==================================================================================================');
+
       telephony.listenIncomingSms(
           onNewMessage: onMessage, onBackgroundMessage: backgroundMessage);
     }
@@ -49,43 +55,28 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Plugin example app'),
-            ),
-            body: FutureBuilder(
-              future: allMessage(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.none) {
-                  return const Center(
-                    child: Text('Loading ...'),
-                  );
-                } else {
-                  return RefreshIndicator(
-                    onRefresh: () async {
-                      setState(() {
-                        messages;
-                      });
-                    },
-                    child: ListView.separated(
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListTile(
-                              leading: const Icon(Icons.markunread),
-                              title: Text((messages[index].address).toString()),
-                              subtitle: Text(
-                                (messages[index].body).toString(),
-                                maxLines: 2,
-                              ),
-                            ),
-                          );
-                        },
-                        separatorBuilder: (context, index) =>
-                            const Divider(color: Colors.black),
-                        itemCount: messages.length),
-                  );
-                }
-              },
-            )));
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body:messages.isEmpty? 
+        const Center(child:Text('Loading...')):
+        ListView.separated(
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: const Icon(Icons.markunread),
+                title: Text((messages[index].address).toString()),
+                subtitle: Text(
+                  (messages[index].body).toString(),
+                  maxLines: 2,
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) =>
+              const Divider(color: Colors.black),
+          itemCount: messages.length),
+    ));
   }
 }
