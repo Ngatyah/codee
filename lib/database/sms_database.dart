6f09171d327a305bd7fad087b7b67c3fb2399426
Ngatyah <davidngatyah@gmail.com>
2021-11-02 07:55:54 +0300

    Create a message model

--- lib/database/sms_database.dart
+++ lib/database/sms_database.dart
@@ -1,3 +1,35 @@
+import 'package:codee/models/messages.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 class SmsDatabase {
-  
+static final SmsDatabase instance = SmsDatabase._init();
+static Database? _database;
+  SmsDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('messages.db');
+    return _database!;
+  }
+  Future<Database> _initDB(String filePath) async {
+    final _dbPath = await getDatabasesPath();
+    final path = join(_dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const textType = 'TEXT NOT NULL';
+
+    await db.execute('''
+    CREATE TABLE $smsDb(
+      ${SmsFields.id} $idType,
+      ${SmsFields.body} $textType,
+      ${SmsFields.title} $textType,
+      ${SmsFields.date} $textType 
+    )
+    ''');
+  }
+
+
 }
---
+++ lib/models/messages.dart
@@ -0,0 +1,15 @@
+const String smsDb = 'messages';
+
+class SmsFields {
+  static final List<String> values = [
+    id,
+    title,
+    body,
+    date
+  ];
+
+  static const String id = '_id';
+  static const String title = 'title';
+  static const String body = 'body';
+  static const String date = 'date';
+}
